a6e92cb9ab04cac35eaf2a2fe1862dc3cbc96b0f
Balli Montero <owurakwakumanu@gmail.com>
2019-04-08 00:17:42 -0800

    making things better

--- src/autorun creator.cpp
+++ src/autorun creator.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : autorun.cpp
-// Author      : Brokah Mozis
-// Version     : 1.0
-// Copyright   : Academic use only
-// Description : create autorun for USB and removable drives in C++
-//============================================================================
-
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -13,23 +5,20 @@ using namespace std;
 
 int main() {
 ofstream myfile("autorun.inf");
-
-string autorunProgram;
+string target;
 
 cout << "\nEnter Autorun target name:\n\n";
-cin >> autorunProgram;
+cin >> target;
 
 string temp = "[autorun]\n\
-;Open="+autorunProgram+"\n\
-ShellExecute="+autorunProgram+"\n\
+;Open="+target+"\n\
+ShellExecute="+target+"\n\
 UseAutoPlay=1";
 
 if(myfile.is_open()){
 	myfile << temp;
 	myfile.close();
-}
-else{
-	cout << "Error, try again.";
+	cout << "...Okay!";
 }
 return 0;
 }
